ae16ddbea2b4c734e43524d9bfca784ec4dc4725
s70c3 <mari.s70c3@gmail.com>
2023-01-22 01:19:45 +0300

    Noise realized.

--- notebooks/utils/weatheraug.py
+++ notebooks/utils/weatheraug.py
@@ -126,45 +126,13 @@ def add_fog(image, coeff=-1):
 
     return image_RGB
 
-def noisy(image, noise_typ):
-   if noise_typ == "gauss":
-      row,col,ch= image.shape
-      mean = 0
-      var = 0.1
-      sigma = var**0.5
-      gauss = np.random.normal(mean,sigma,(row,col,ch))
-      gauss = gauss.reshape(row,col,ch)
-      noisy = image + gauss
-      return noisy
-   elif noise_typ == "s&p":
-      row,col,ch = image.shape
-      s_vs_p = 0.5
-      amount = 0.004
-      out = np.copy(image)
-      # Salt mode
-      num_salt = np.ceil(amount * image.size * s_vs_p)
-      coords = [np.random.randint(0, i - 1, int(num_salt))
-              for i in image.shape]
-      out[coords] = 1
+def noisy(image, noise_type='gaussian'):
+    if noise_type not in ['gaussian', 'poisson', 's&p', 'speckle']:
+        raise Exception('Noise type should be one of these: gaussian, poisson, s&p, speckle.')
+    from skimage.util import random_noise
+    noise_img = random_noise(image, mode=noise_type)
 
-      # Pepper mode
-      num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-      coords = [np.random.randint(0, i - 1, int(num_pepper))
-              for i in image.shape]
-      out[coords] = 0
-      return out
-   elif noise_typ == "poisson":
-      vals = len(np.unique(image))
-      vals = 2 ** np.ceil(np.log2(vals))
-      noisy = np.random.poisson(image * vals) / float(vals)
-      return noisy
-   elif noise_typ =="speckle":
-      row,col,ch = image.shape
-      gauss = np.random.randn(row,col,ch)
-      gauss = gauss.reshape(row,col,ch)
-      noisy = image + image * gauss
-      return noisy
-
+    return np.array(255*noise_img, dtype = 'uint8')
 
 def fill(img, h, w):
     img = cv2.resize(img, (h, w), cv2.INTER_CUBIC)
