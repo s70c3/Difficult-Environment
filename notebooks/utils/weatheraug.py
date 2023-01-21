b3a2cbf67dc1fea41dbdffd6481bf7f4ab42feef
s70c3 <mari.s70c3@gmail.com>
2023-01-22 01:31:59 +0300

    Added sun flare.

--- notebooks/utils/weatheraug.py
+++ notebooks/utils/weatheraug.py
@@ -134,6 +134,64 @@ def noisy(image, noise_type='gaussian'):
 
     return np.array(255*noise_img, dtype = 'uint8')
 
+import math
+err_flare_circle_count="Numeric value between 0 and 20 is allowed"
+def flare_source(image, point,radius,src_color):
+    overlay= image.copy()
+    output= image.copy()
+    num_times=radius//10
+    alpha= np.linspace(0.0,1,num= num_times)
+    rad= np.linspace(1,radius, num=num_times)
+    for i in range(num_times):
+        cv2.circle(overlay,point, int(rad[i]), src_color, -1)
+        alp=alpha[num_times-i-1]*alpha[num_times-i-1]*alpha[num_times-i-1]
+        cv2.addWeighted(overlay, alp, output, 1 -alp ,0, output)
+    return output
+
+def add_sun_flare_line(flare_center,angle, w):
+    x=[]
+    y=[]
+    i=0
+    for rand_x in range(0,w,10):
+        rand_y= math.tan(angle)*(rand_x-flare_center[0])+flare_center[1]
+        x.append(rand_x)
+        y.append(2*flare_center[1]-rand_y)
+    return x,y
+
+def add_sun_process(image, no_of_flare_circles,flare_center,src_radius,x,y,src_color):
+    overlay= image.copy()
+    output= image.copy()
+    imshape=image.shape
+    for i in range(no_of_flare_circles):
+        alpha=random.uniform(0.05,0.2)
+        r=random.randint(0, len(x)-1)
+        rad=random.randint(1, imshape[0]//100-2)
+        cv2.circle(overlay,(int(x[r]),int(y[r])), rad*rad*rad, (random.randint(max(src_color[0]-50,0), src_color[0]),random.randint(max(src_color[1]-50,0), src_color[1]),random.randint(max(src_color[2]-50,0), src_color[2])), -1)
+        cv2.addWeighted(overlay, alpha, output, 1 - alpha,0, output)
+    output= flare_source(output,(int(flare_center[0]),int(flare_center[1])),src_radius,src_color)
+    return output
+def add_sun_flare(image,flare_center=None, angle=None, no_of_flare_circles=8,src_radius=400, src_color=(255,255,255)):
+    if angle:
+        angle=angle%(2*math.pi)
+    if not(0 <= no_of_flare_circles <= 20):
+        raise Exception(err_flare_circle_count)
+
+    h, w=image.shape[:2]
+    if angle is None:
+        angle=random.uniform(0,2*math.pi)
+        if angle==math.pi/2:
+            angle=0
+
+    if flare_center is None:
+        flare_center=(random.randint(0,w),random.randint(0,h//2))
+
+    x,y= add_sun_flare_line(flare_center,angle, w)
+    output= add_sun_process(image, no_of_flare_circles,flare_center,src_radius,x,y,src_color)
+    image_RGB = output
+    return image_RGB
+
+
+
 def fill(img, h, w):
     img = cv2.resize(img, (h, w), cv2.INTER_CUBIC)
     return img
