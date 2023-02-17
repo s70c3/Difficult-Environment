1e3e7144e2964a791752cf96a0c8f2e1598c87cd
s70c3 <mari.s70c3@gmail.com>
2023-02-17 23:51:40 +0300

    Copy function.

--- notebooks/utils/apply.py
+++ notebooks/utils/apply.py
@@ -33,3 +33,27 @@ def apply_to_stereo(base_dir, target_dir, n=2, resize_size = None):
         img_l_aug, img_r_aug = random_aug([img_l, img_r], n=n)
         cv2.imwrite(os.path.join(target_dir, 'left', filename), img_l_aug)
         cv2.imwrite(os.path.join(target_dir, 'right', filename), img_r_aug)
+
+
+def random_combine(first_dir, second_dir, target_dir, var = 2):
+    import os
+    import shutil
+    d = os.listdir(os.path.join(first_dir, 'left'))
+    for filename in d:
+        if random.randint(0, var)==0:
+            shutil.copyfile(os.path.join(first_dir, 'left', filename), os.path.join(target_dir, 'left', filename))
+            shutil.copyfile(os.path.join(first_dir, 'right', filename), os.path.join(target_dir, 'right', filename))
+        else:
+            shutil.copyfile(os.path.join(second_dir, 'left', filename), os.path.join(target_dir, 'left', filename))
+            shutil.copyfile(os.path.join(second_dir, 'right', filename), os.path.join(target_dir, 'right', filename))
+
+def regular_combine(first_dir, second_dir, target_dir, split = 0.5):
+    import os
+    import shutil
+    d = os.listdir(os.path.join(first_dir, 'left'))
+    for i in range(int(len(d)*split)):
+        shutil.copyfile(os.path.join(first_dir, 'left', f'{i}.png'), os.path.join(target_dir, 'left', f'{i}.png'))
+        shutil.copyfile(os.path.join(first_dir, 'right', f'{i}.png'), os.path.join(target_dir, 'right', f'{i}.png'))
+    for i in range(int(len(d) * split), len(d)):
+        shutil.copyfile(os.path.join(second_dir, 'left', f'{i}.png'), os.path.join(target_dir, 'left', f'{i}.png'))
+        shutil.copyfile(os.path.join(second_dir, 'right', f'{i}.png'), os.path.join(target_dir, 'right', f'{i}.png'))
